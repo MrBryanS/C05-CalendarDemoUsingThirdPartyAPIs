3da3fedca8e4403c1a7784e2c396508def709a0f
Bryan Stauning <bryanstauning@gmail.com>
2023-06-08 00:05:35 -0500

    Local storage is working and is not lost on reload or restart

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,13 +4,32 @@
 
 
 $(function () {
-  var activityList = [];
+//check the time and color code the time blocks
+ 
+
+
+
+
+
+//////
+  var activityList = JSON.parse(localStorage.getItem("scheduledEvents")) || [];
+  // how do I populate the text area with the text from local storage?
+  activityList.forEach(item => {
+    console.log(item.time);
+    //target the time block with the id of the time in the object
+    var timeBlock = $("#" + item.time);
+    //put the text in the text area
+    timeBlock.children(".description").val(item.text);
+        
+    });
+
+
   //add today's date to the header:
   $("#currentDay").text("Today's date is: " + dayjs().format('MM/DD/YYYY'));
 
   //add listener to save button click event
   $(".saveBtn").on("click", function (e) {
-    console.log (this);
+    console.log(this);
     //get the value of the text area
     var text = $(this).siblings(".description").val();
     console.log(text);
@@ -23,16 +42,16 @@ $(function () {
     };
 
     //save the text to local storage
-    if(activityList.find( obj => obj.time == time)) {
+    if (activityList.find(obj => obj.time == time)) {
       // find the index of the object with the time in my array
       var index = activityList.findIndex(obj => obj.time == time);
       // remove the object from the array, then add the new object
-      activityList.splice(index,1);
+      activityList.splice(index, 1);
     }
 
     activityList.push(timeObject);
 
-    localStorage.setItem("saveStuff",JSON.stringify(activityList));
+    localStorage.setItem("scheduledEvents", JSON.stringify(activityList));
   });
 
 
