172e0bf18fd794e54e65ac75b48ff7e921c4ff44
Bryan Stauning <bryanstauning@gmail.com>
2023-06-07 21:08:43 -0500

    First commit on new branch wednesday

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,16 +4,35 @@
 
 
 $(function () {
-
+  var activityList = [];
   //add today's date to the header:
   $("#currentDay").text("Today's date is: " + dayjs().format('MM/DD/YYYY'));
 
   //add listener to save button click event
-  $(".saveBtn").on("click", function () {
+  $(".saveBtn").on("click", function (e) {
+    console.log (this);
     //get the value of the text area
     var text = $(this).siblings(".description").val();
+    console.log(text);
     //get the id of the time block
     var time = $(this).parent().attr("id");
+
+    var timeObject = {
+      time: time,
+      text: text,
+    };
+
+    //save the text to local storage
+    if(activityList.find( obj => obj.time == time)) {
+      // find the index of the object with the time in my array
+      var index = activityList.findIndex(obj => obj.time == time);
+      // remove the object from the array, then add the new object
+      activityList.splice(index,1);
+    }
+
+    activityList.push(timeObject);
+
+    localStorage.setItem("saveStuff",JSON.stringify(activityList));
   });
 
 
