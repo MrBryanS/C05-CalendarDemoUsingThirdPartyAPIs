e5410f8dce6fd53971b13da37cbd45b28bb963aa
Bryan Stauning <bryanstauning@gmail.com>
2023-06-10 16:58:19 -0500

    Added functionality to allow before 9am after 5pm proper past current future calculations.  Also added current time on last refresh to main screen.

--- README.md
+++ README.md
@@ -2,5 +2,20 @@ This application features dymaically updated HTML and CSS using jQuery and the D
 
 Starter code used for this project is here: https://github.com/coding-boot-camp/crispy-octo-meme
 
+Notes:
+    Past hours are grey in color
+    Current hour is blue
+    Future hours are green
 
+    The current time as of last refresh used to calculate pasts/present/future states is shown with today's date
 
+    If the current time is prior to 9am, all timeblock divs are future
+    If the current time is after 5pm, all timeblock divs are past
+    Otherwise the divs show current hour in blue, past grey and future green.
+        
+    Click on the the save button to the right of the hour to save that hour's content.
+    Saved content will persist in local storage
+
+    To clear an hour: delete all text and click save
+
+![Alt text](image.png)
--- assets/script/script.js
+++ assets/script/script.js
@@ -4,69 +4,64 @@
 
 
 $(function () {
-//check the time and color code the time blocks
- 
 
+  //loop through the time blocks and apply the appropriate class
+  //to the time block based on the current time
+  //var currentHour = dayjs().hour();
+  var currentHour = dayjs().hour();
+  var currentHourNumber = parseInt(currentHour);
+  console.log(currentHour,typeof currentHour);
 
+  //loop through divs with timeBlock class
 
-//loop through the time blocks and apply the appropriate class
-//to the time block based on the current time
-var currentHour = dayjs().hour();
-var currentHourText = currentHour.toString();
-console.log(currentHourText, typeof currentHourText);
-//var isCurrentHour = false;
-//var isPastHour = false;
-//var isFutureHour = false;
-//if ("15"==="15") {var isCurrentHour = true };
-//console.log(isCurrentHour, typeof isCurrentHour);
+  $(".time-block").each(function () {
+    var timeBlock = $(this).attr("id");
+    var timeBlockNumber = parseInt(timeBlock);
+    console.log(timeBlockNumber,typeof timeBlockNumber);
 
-
-
-
-$(".time-block").each(function () {
-  var timeBlock = $(this).attr("id");
-  console.log(timeBlock,typeof timeBlock);
-
-  if (timeBlock < currentHourText) {
-    console.log("past");
-    $(this).addClass("past");
-  }
-  else if (timeBlock === currentHourText) {
-    console.log("present");
-
-    $(this).addClass("present");
-  }
-  else if (timeBlock > currentHourtText){
-    console.log("future");
-    $(this).addClass("future");
-  }
-});
+    //If the current hour is less than the time block, add the future class
+    if (currentHourNumber < timeBlockNumber) {
+      $(this).addClass("future");
+    }
+    //if the current hour is greater than the time block, add the past class
+    else if (currentHourNumber > timeBlockNumber ) {
+      $(this).addClass("past");
+    }
+    //if the current hour is equal to the time block, add the present class
+    else if (timeBlockNumber === currentHourNumber) {
+       $(this).addClass("present");
+    }
+    // else if (timeBlockNumber > currentHourNumber) {
+    //   $(this).addClass("future");
+    // }
+  });
 
   //get the hour from the id
-  
+
 
 
   var activityList = JSON.parse(localStorage.getItem("scheduledEvents")) || [];
   // how do I populate the text area with the text from local storage?
   activityList.forEach(item => {
-    //console.log(item.time);
     //target the time block with the id of the time in the object
     var timeBlock = $("#" + item.time);
     //put the text in the text area
     timeBlock.children(".description").val(item.text);
-        
-    });
+
+  });
 
 
   //add today's date to the header:
   $("#currentDay").text("Today's date is: " + dayjs().format('MM/DD/YYYY'));
+  $("#currentTime").text("Time on last page refresh is:  " + dayjs().format('hh:mm A'));
 
-  //add listener to save button click event
-  $(".saveBtn").on("click", function (e) {
-    console.log(this);
+  //pull the save function out of the click envent
+  //Fuction delcaration
+  function save() {
+    //console.log(this);
     //get the value of the text area
     var text = $(this).siblings(".description").val();
-    console.log(text);
+    //console.log(text);
     //get the id of the time block
     var time = $(this).parent().attr("id");
 
@@ -74,6 +69,7 @@ $(".time-block").each(function () {
       time: time,
       text: text,
     };
+    //console.log(timeObject);
 
     //save the text to local storage
     if (activityList.find(obj => obj.time == time)) {
@@ -86,7 +82,22 @@ $(".time-block").each(function () {
     activityList.push(timeObject);
 
     localStorage.setItem("scheduledEvents", JSON.stringify(activityList));
-  });
+
+
+
+  }
+
+
+  //add listener to save button click event
+  $(".saveBtn").on("click", save);
+
+  //future feature: add listener to save button keypress event
+  // $(".saveBtn").on("keypress", function (e) {
+  //   if (e.which == 13) {
+  //     save();
+  //   }
+  // })
+
 
 });
 
